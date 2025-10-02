5d000c8bb610e508b37f1848e48c6cb6d94be08e
SmileYik <miskyle@outlook.com>
2025-10-02 23:45:40 +0800

    add SimpleHome package

---
+++ repo/packages/SimpleHome/package.lua
@@ -0,0 +1,14 @@
+return {
+    name = "SimpleHome",
+    version = "0.0.1",
+    authors = { "SmileYik" },
+    description = "Allow player set home and teleport to home",
+    luaVersion = nil,
+    dependPackages = {
+        "json"
+    },
+    dependPlugins = {},
+    main = "SimpleHome.lua",
+    runnable = true,
+    reason = ""
+}
